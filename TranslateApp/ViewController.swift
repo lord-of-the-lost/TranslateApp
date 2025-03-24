818d3538bf4b24d579cfd2f1c66f4b6a056290cc
lord-of-the-lost <ignatov.99@yandex.ru>
2025-03-24 13:51:41 +0300

    Initial Commit

---
+++ TranslateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslateApp
+//
+//  Created by Николай Игнатов on 24.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
